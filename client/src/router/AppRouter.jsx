262cecbd085ca01ab1e6a14485aa28c51e4b8f06
JanethHdez1 <20223tn061@utez.edu.mx>
2024-02-26 11:52:29 -0600

    todos mis archivos

---
+++ client/src/router/AppRouter.jsx
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react';
+import SignInPage from '../modules/auth/SignInPage';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import AuthContext from '../config/context/auth-context';
+import AdminLayout from '../modules/admin/AdminLayout';
+
+const AppRouter = () => {
+  const userRole = localStorage.getItem("userRole");
+  const username = localStorage.getItem("username");
+  const { user } = useContext(AuthContext);
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        {
+          //Publico
+        }
+        {user.signed ? (
+        <>
+            <Route path='/' element={<AdminLayout username={username} userRole={userRole} />} >
+              <Route path='admin' element={<> !Bienvenido {username}, con Rol: {userRole}! </>} />
+              <Route path='user' element={<> !Bienvenido {username}, con Rol: {userRole}! </>} />
+              <Route path='client' element={<> !Bienvenido {username}, con Rol: {userRole}! </>} />
+            </Route>
+          </>
+          ) :(
+            
+        <Route path='/' element={<SignInPage />} />
+          )
+        }
+       <Route path='/*' element={<>404 NOT FOUND</>} />
+      </>
+    )
+  );
+  return <RouterProvider router={router}/>;
+}; 
+
+export default AppRouter
